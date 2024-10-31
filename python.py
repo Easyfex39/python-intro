3464c2ec9e4071e479ae4ee8433a62df2c5dc8e6
easyfex <idahosaefosa19@gmail.com>
2024-10-31 09:35:10 -0400

    my fill

---
+++ python.py
@@ -0,0 +1,44 @@
+a=10
+b=20
+c=a+b
+print('this is my result' ,c)
+
+a=50
+b=40
+c=90
+f=a+b+c
+print('this is my result ',f)
+
+b=30
+a=90
+f=a-b
+print('this is my result' ,f)
+
+a=400
+b=700
+f=b-a
+print(f)
+
+# write a python code that will add this together
+#91 81 71.
+
+x=91
+y=81
+w=71
+z=y+x+w
+print(z)
+
+w=90
+x=50
+t=w*x
+print(t) 
+
+t=30
+y=40
+f=t*y/10
+print(f)
+
+r=800
+t=500
+w=r+t-99
+print(w)
